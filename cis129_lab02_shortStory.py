0d48391e17bcadb8da93214fe5526a9a343d5632
RyanC-Pima <158860768+RyanC-Pima@users.noreply.github.com>
2024-02-04 16:47:56 -0700

    Update cis129_lab02_shortStory.py
    Adding short story for CIS129 Module2

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Aaron C (My son, who is also interested in programming and creative writing)
+# My first Python lab in my CIS129 course
+# Prints a short story by Aaron C
+
+print("Normally when you throw a rock into the water, it makes a big splash before sinking to the bottom of said body of water.")
+print("But for some reason when he throws it, it floats.")
+print("Rocks with bodies of water don't just float, not unless there's something holding it up, but the water is clear and there's nothing there. ")
+print("He bends down and grabs another rock, it's bigger than the last pebble he had thrown, and this time he knows he throws it harder.")
+print("Like the previous stone, it floats, and no matter how much he squints his eyes and searches, there's nothing there.")
